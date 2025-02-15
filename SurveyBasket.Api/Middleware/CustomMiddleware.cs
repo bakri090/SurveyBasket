cfb156eda9c1cff3d5d950f67a93e47f06599b70
abubaker <bakri090200@gmail.com>
2025-02-15 14:02:19 +0300

    Add project files.

---
+++ SurveyBasket.Api/Middleware/CustomMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace SurveyBasket.Api.Middleware
+{
+	public class CustomMiddleware
+	{
+		private readonly ILogger<CustomMiddleware> _logger;
+		private readonly RequestDelegate _next;
+
+		public CustomMiddleware(ILogger<CustomMiddleware> logger, RequestDelegate next)
+		{
+			_logger = logger;
+			_next = next;
+		}
+
+		public async Task InvokeAsync(HttpContext context)
+		{
+			_logger.LogInformation("Processing request");
+			await _next(context);
+			_logger.LogError("Processing response");
+		}
+	}
+}
---
+++ SurveyBasket.Api/Middleware/CustomMiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyBasket.Api.Middleware
+{
+	public static class CustomMiddlewareExtension 
+	{
+		public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<CustomMiddleware>();
+		} 
+	}
+}
